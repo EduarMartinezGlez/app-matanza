637fb69bcad3b1bb39ea8ebf0454764e74f719ed
Eduard Martinz <emartinezgonzalez2012@gmail.com>
2021-12-09 21:20:27 +0100

    commit

---
+++ config/config.js
@@ -0,0 +1,15 @@
+const config ={
+    env: process.env.NODE_ENV || 'dev',
+    port: process.env.PORT || 3000,
+    dbUser: process.env.DB_USER,
+    dbPassword: process.env.DB_PASSWORD,
+    dbHost: process.env.DB_HOST,
+    dbName: process.env.DB_NAME,
+    dbPort: process.env.DB_PORT,
+    dbUrl: process.env.DB_URL,
+    sercret: process.env.JWT_SERCRET,
+    UserEmail: process.env.USEREMAIL,
+    EmailPass:process.env.EMAILPASS
+}
+
+module.exports = {config}
